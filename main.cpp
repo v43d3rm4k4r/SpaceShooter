0ba764d8fb925b63050c3acbacfa21667159dbb2
Daniil Kuprianov <vaedermakar@mail.ru>
2020-08-03 22:07:38 +0300

    field almost completed

--- main.cpp
+++ main.cpp
@@ -11,17 +11,48 @@ static constexpr auto MINIMUM_WIDTH = 5;
 static constexpr auto MAXIMUM_HEIGHT = 50;
 static constexpr auto MINIMUM_HEIGHT = 10;
 
-
 static constexpr auto DEFAULT_GLOBAL_WIDTH = 15;
 static constexpr auto DEFAULT_GLOBAL_HEIGHT = 10;
 
 // TODO: UNICODE TEXT
 // TODO: 2 PLAYERS (Qt)
 //===============================================================================
+struct Point
+{
+	size_t x;
+	size_t y;
+};
+//===============================================================================
 class GameEngine
 {
-	static size_t _width;
-	static size_t _height;
+	static size_t _global_width;
+	static size_t _global_height;
+	static size_t _score;
+
+	class Player
+	{
+		Point coords;
+	public:
+		Player()
+			: coords{_global_width / 2, _global_height - 2}
+			{}
+
+		size_t x() const
+		{
+			return coords.x;
+		}
+
+		size_t y() const
+		{
+			return coords.y;
+		}
+
+		void shoot()
+		{
+
+		}
+	};
+
 
 	static void showSettings()
 	{
@@ -39,10 +70,10 @@ class GameEngine
 					system("cls");
 					cout << "Enter field width (from " << MINIMUM_WIDTH << " to " << MAXIMUM_WIDTH << ")" << endl;
 					cout << ">";
-					while (scanf(" %u", &_width) != 1 || _width > MAXIMUM_WIDTH || _width < MINIMUM_WIDTH)
+					while (scanf(" %u", &_global_width) != 1 || _global_width > MAXIMUM_WIDTH || _global_width < MINIMUM_WIDTH)
 					{
 						cout << "Invalid input." << endl;
-						_width = DEFAULT_GLOBAL_WIDTH;
+						_global_width = DEFAULT_GLOBAL_WIDTH;
 						system("pause>0");
 						system("cls");
 						cout << "Enter field width (from " << MINIMUM_WIDTH << " to " << MAXIMUM_WIDTH << ")" << endl;
@@ -58,10 +89,10 @@ class GameEngine
 					system("cls");
 					cout << "Enter field height (from " << MINIMUM_HEIGHT << " to " << MAXIMUM_HEIGHT << ")" << endl;
 					cout << ">";
-					while (scanf(" %u", &_height) != 1 || _height > MAXIMUM_HEIGHT || _height < MINIMUM_HEIGHT)
+					while (scanf(" %u", &_global_height) != 1 || _global_height > MAXIMUM_HEIGHT || _global_height < MINIMUM_HEIGHT)
 					{
 						cout << "Invalid input." << endl;
-						_height = DEFAULT_GLOBAL_HEIGHT;
+						_global_height = DEFAULT_GLOBAL_HEIGHT;
 						system("pause>0");
 						system("cls");
 						cout << "Enter field height (from " << MINIMUM_HEIGHT << " to " << MAXIMUM_HEIGHT << ")" << endl;
@@ -83,11 +114,34 @@ class GameEngine
 	//===============================================================================
 	static void printField()
 	{
-		for (size_t y = 0; y < _height; ++y)
+		Player player;
+
+		for (size_t y = 0; y < _global_height; ++y)
 		{
-			for (size_t x = 0; x < _width; ++x)
+			for (size_t x = 0; x < _global_width; ++x)
 			{
-				if (y == 0 || x == 0 || y == _height-1 || x == _width-1)
+				if (player.x() == x && player.y()-1 == y)
+				{
+					cout << "^";
+					continue;
+				}
+				if (player.x() == x && player.y() == y)
+				{
+					cout << "v";
+					continue;
+				}
+				if (player.x()-1 == x && player.y() == y)
+				{
+					cout << "<";
+					continue;
+				}
+				if (player.x()+1 == x && player.y() == y)
+				{
+					cout << ">";
+					continue;
+				}
+
+				if (y == 0 || x == 0 || y == _global_height-1 || x == _global_width-1)
 				{
 					cout << '#';
 				}
@@ -98,13 +152,24 @@ class GameEngine
 			}
 			cout << endl;
 		}
+		cout << endl << endl;
+
+		for (int i = 0; i < _global_width - 5; ++i) cout << ' ';
+		cout << "SCORE = " << _score << endl;
+		system("cls");
+
 	}
 	//===============================================================================
 	static int initMainLoop()
 	{
 	    system("cls");
 
-		printField();
+	    while(true)
+	    {
+	    	printField();
+	    }
+
+
 
 		return 0;
 	}
@@ -143,8 +208,9 @@ public:
 	}
 };
 //===============================================================================
-size_t GameEngine::_width = DEFAULT_GLOBAL_WIDTH;
-size_t GameEngine::_height = DEFAULT_GLOBAL_HEIGHT;
+size_t GameEngine::_global_width = DEFAULT_GLOBAL_WIDTH;
+size_t GameEngine::_global_height = DEFAULT_GLOBAL_HEIGHT;
+size_t GameEngine::_score = 0;
 //===============================================================================
 int main()
 {
